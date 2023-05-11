8d756b9cd3103fff80e7bfadc7f44d315ff7c5f9
whimsee <jbtenmatay@gmail.com>
2023-05-11 01:46:07 -0700

    add sample

--- examples/pmw3360_simpletest.py
+++ examples/pmw3360_simpletest.py
@@ -2,3 +2,32 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023 Jerico Tenmatay
 #
 # SPDX-License-Identifier: Unlicense
+
+import PMW3360
+import board
+from digitalio import DigitalInOut, Direction
+
+def delta(value):
+if value & 0x8000:
+    return -(~value & 0x7fff)
+else:
+    return value & 0x7fff
+
+sensor = PMW3360.PMW3360(board.CLK, board.MOSI, board.MISO, board.D10)
+
+mt_pin = DigitalInOut(board.A0)
+mt_pin.direction = Direction.INPUT
+
+sensor.begin()
+
+while True:
+
+    data = sensor.read_burst()
+
+    dx = delta(data["dx"])
+    dy = delta(data["dy"])
+
+    if mt_pin.value == 0:
+    print(dx)
+    print(dy)
+    print("")
